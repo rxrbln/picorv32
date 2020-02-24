5ca901b3bbd2b3abc7e16079d3f95aff82b67125
René Rebe <rene@exactcode.com>
2020-02-24 13:58:51 +0100

    implemented ulx3s bi-directional quad-spi, fixed iomem read, incl. led read not to stall RV32 core, stereo DAC, etc.

--- picosoc/ulx3s.v
+++ picosoc/ulx3s.v
@@ -33,12 +33,20 @@ module attosoc (
 	output [3:0] audio_r,
 	output [3:0] gpdi_dp, gpdi_dn,
 
+	input flash_sck,
 	output flash_csn,
-	output flash_clk,
-	input flash_io0,
-	input flash_io1,
-	input flash_io2,
-	input flash_io3,
+	output flash_io0_oe,
+	output flash_io1_oe,
+	output flash_io2_oe,
+	output flash_io3_oe,
+	output flash_io0_do,
+	output flash_io1_do,
+	output flash_io2_do,
+	output flash_io3_do,
+	input  flash_io0_di,
+	input  flash_io1_di,
+	input  flash_io2_di,
+	input  flash_io3_di,
 );
    	reg [5:0] reset_cnt = 0;
 	wire resetn = &reset_cnt;
@@ -47,12 +55,12 @@ module attosoc (
 		reset_cnt <= reset_cnt + !resetn;
 	end
 
-	parameter integer MEM_WORDS = 8192;
+	parameter integer MEM_WORDS = 32768;
 	parameter [31:0] STACKADDR = 32'h 0000_0000 + (4*MEM_WORDS);       // end of memory
-	parameter [31:0] PROGADDR_RESET = 32'h 0000_0000;       // start of memory
+	parameter [31:0] PROGADDR_RESET = 32'h 0020_0000; // 2 MB into flash
 
 	reg [31:0] ram [0:MEM_WORDS-1];
-	initial $readmemh("ulx3s_fw.hex", ram);
+	//initial $readmemh("ulx3s_fw.hex", ram);
         reg [31:0] 	   ram_rdata;
 	reg ram_ready;
 
@@ -68,102 +76,47 @@ module attosoc (
         wire dac_ready;
    
 	always @(posedge clk)
-        begin
-		ram_ready <= 1'b0;
+          begin
+	     ram_ready <= mem_valid && !mem_ready && mem_addr < 4*MEM_WORDS;
 		if (mem_addr[31:24] == 8'h00 && mem_valid) begin
 			if (mem_wstrb[0]) ram[mem_addr[23:2]][7:0] <= mem_wdata[7:0];
 			if (mem_wstrb[1]) ram[mem_addr[23:2]][15:8] <= mem_wdata[15:8];
 			if (mem_wstrb[2]) ram[mem_addr[23:2]][23:16] <= mem_wdata[23:16];
 			if (mem_wstrb[3]) ram[mem_addr[23:2]][31:24] <= mem_wdata[31:24];
-
 			ram_rdata <= ram[mem_addr[23:2]];
-			ram_ready <= 1'b1;
 		end
         end
 
-   	wire flash_io0_oe, flash_io0_do, flash_io0_di;
-	wire flash_io1_oe, flash_io1_do, flash_io1_di;
-	wire flash_io2_oe, flash_io2_do, flash_io2_di;
-	wire flash_io3_oe, flash_io3_do, flash_io3_di;
-
-        BB flash_io_buf[3:0] (
-	   .I({flash_io3_di, flash_io2_di, flash_io1_di, flash_io0_di}),
-	   .T({flash_io3_oe, flash_io2_oe, flash_io1_oe, flash_io0_oe}),
-           .O({flash_io3_do, flash_io2_do, flash_io1_do, flash_io0_do}),
-	   .B({flash_io3, flash_io2, flash_io1, flash_io0})
-	);
-
+ 
 	wire iomem_valid;
 	reg iomem_ready;
 	wire [31:0] iomem_addr;
 	wire [31:0] iomem_wdata;
 	wire [3:0] iomem_wstrb;
-	wire [31:0] iomem_rdata;
+	reg [31:0] iomem_rdata;
 
 	assign iomem_valid = mem_valid && (mem_addr[31:24] > 8'h 01);
 	assign iomem_wstrb = mem_wstrb;
 	assign iomem_addr = mem_addr;
 	assign iomem_wdata = mem_wdata;
 
-   	wire spimemio_cfgreg_sel = mem_valid && (mem_addr == 32'h 0200_0000);
-	wire [31:0] spimemio_cfgreg_do;
+        wire       spimem_ready;
+        wire [31:0] spimem_rdata;
 
-	wire        simpleuart_reg_div_sel = mem_valid && (mem_addr == 32'h 0200_0004);
-	wire [31:0] simpleuart_reg_div_do;
-
-	wire        simpleuart_reg_dat_sel = mem_valid && (mem_addr == 32'h 0200_0008);
-	wire [31:0] simpleuart_reg_dat_do;
-	wire simpleuart_reg_dat_wait;
-
-	always @(posedge clk) begin
-	   iomem_ready <= 1'b0;
-	   if (iomem_valid && iomem_wstrb[0] && mem_addr == 32'h 03000000) begin
-	      led <= iomem_wdata[7:0];
-	      iomem_ready <= 1'b1;
-	   end else if (vgamem_sel && !iomem_ready) begin
-	      iomem_ready <= vgamem_ready;
-	      iomem_rdata <= vgamem_rdata;
-	   end else if (iomem_valid && dac_sel) begin
-	      iomem_ready <= dac_ready;
-	   end
-	end
-
-
-	assign mem_ready = (iomem_valid && iomem_ready) ||
-			   spimem_ready || spimemio_cfgreg_sel ||
-	                   simpleuart_reg_div_sel || (simpleuart_reg_dat_sel && !simpleuart_reg_dat_wait) ||
-										 ram_ready;
-
-	assign mem_rdata = simpleuart_reg_div_sel ? simpleuart_reg_div_do :
-			   simpleuart_reg_dat_sel ? simpleuart_reg_dat_do :
-			   spimem_ready ? spimem_rdata :
-			   spimemio_cfgreg_sel ? spimemio_cfgreg_do :
- 			   ram_rdata;
-
-	picorv32 #(
-		.STACKADDR(STACKADDR),
-		.PROGADDR_RESET(PROGADDR_RESET),
-		.PROGADDR_IRQ(32'h 0000_0000),
-		.BARREL_SHIFTER(1),
-		.COMPRESSED_ISA(0),
-		.ENABLE_MUL(1),
-		.ENABLE_DIV(1),
-		.ENABLE_IRQ(0),
-		.ENABLE_IRQ_QREGS(0)
-	) cpu (
-		.clk         (clk        ),
-		.resetn      (resetn     ),
-		.mem_valid   (mem_valid  ),
-		.mem_instr   (mem_instr  ),
-		.mem_ready   (mem_ready  ),
-		.mem_addr    (mem_addr   ),
-		.mem_wdata   (mem_wdata  ),
-		.mem_wstrb   (mem_wstrb  ),
-		.mem_rdata   (mem_rdata  )
-	);
-
-   	wire spimem_ready;
-	wire [31:0] spimem_rdata;
+        /*
+        flashmem flash_i (
+           .clk(clk),
+           .reset(!resetn),
+           .valid(mem_valid && mem_addr >= 4*MEM_WORDS && mem_addr < 32'h 0200_0000),
+           .addr(mem_addr[23:0]),
+           .ready(spimem_ready),
+           .rdata(spimem_rdata),
+	   
+           .spi_cs(flash_csn),
+           .spi_sclk(flash_sck),
+           .spi_mosi(flash_mosi),
+           .spi_miso(flash_miso),
+        );*/
 
    	spimemio spimemio (
 		.clk    (clk),
@@ -173,8 +126,8 @@ module attosoc (
 		.addr   (mem_addr[23:0]),
 		.rdata  (spimem_rdata),
 
-		.flash_csb    (flash_csn   ), // TODO: csn vs. vsb?
-		.flash_clk    (flash_clk   ),
+		.flash_csb    (flash_csn   ),
+		.flash_clk    (flash_sck   ),
 
 		.flash_io0_oe (flash_io0_oe),
 		.flash_io1_oe (flash_io1_oe),
@@ -196,6 +149,67 @@ module attosoc (
 		.cfgreg_do(spimemio_cfgreg_do)
 	);
 
+	wire spimemio_cfgreg_sel = mem_valid && (mem_addr == 32'h 0200_0000);
+	wire [31:0] spimemio_cfgreg_do;
+
+	wire        simpleuart_reg_div_sel = mem_valid && (mem_addr == 32'h 0200_0004);
+	wire [31:0] simpleuart_reg_div_do;
+
+	wire        simpleuart_reg_dat_sel = mem_valid && (mem_addr == 32'h 0200_0008);
+	wire [31:0] simpleuart_reg_dat_do;
+	wire simpleuart_reg_dat_wait;
+
+	always @(posedge clk) begin
+	   iomem_ready <= 1'b0;
+	   if (iomem_valid && mem_addr == 32'h 03000000) begin
+	      if (iomem_wstrb[0]) led <= iomem_wdata[7:0];
+	      iomem_ready <= 1'b1;
+	      iomem_rdata <= led;
+	   end else if (vgamem_sel) begin
+	      iomem_ready <= vgamem_ready;
+	      iomem_rdata <= vgamem_rdata;
+	   end else if (dac_sel) begin
+	      iomem_ready <= dac_ready;
+	   end
+	end
+
+
+	assign mem_ready = (iomem_valid && iomem_ready) ||
+			   spimem_ready || spimemio_cfgreg_sel ||
+	                   simpleuart_reg_div_sel ||
+			   (simpleuart_reg_dat_sel && !simpleuart_reg_dat_wait) ||
+			   ram_ready;
+
+	assign mem_rdata = (iomem_valid && iomem_ready) ? iomem_rdata :
+			   simpleuart_reg_div_sel ? simpleuart_reg_div_do :
+			   simpleuart_reg_dat_sel ? simpleuart_reg_dat_do :
+			   spimem_ready ? spimem_rdata :
+			   spimemio_cfgreg_sel ? spimemio_cfgreg_do :
+ 			   ram_rdata;
+
+	picorv32 #(
+		.STACKADDR(STACKADDR),
+		.PROGADDR_RESET(PROGADDR_RESET),
+		.PROGADDR_IRQ(32'h 0000_0000),
+		.BARREL_SHIFTER(1),
+		.COMPRESSED_ISA(1),
+		.ENABLE_MUL(1),
+		.ENABLE_DIV(1),
+		.ENABLE_IRQ(0),
+		.ENABLE_IRQ_QREGS(0)
+	) cpu (
+		.clk         (clk        ),
+		.resetn      (resetn     ),
+		.mem_valid   (mem_valid  ),
+		.mem_instr   (mem_instr  ),
+		.mem_ready   (mem_ready  ),
+		.mem_addr    (mem_addr   ),
+		.mem_wdata   (mem_wdata  ),
+		.mem_wstrb   (mem_wstrb  ),
+		.mem_rdata   (mem_rdata  ),
+	);
+
+
 	simpleuart simpleuart (
 		.clk         (clk         ),
 		.resetn      (resetn      ),
@@ -211,15 +225,15 @@ module attosoc (
 		.reg_dat_re  (simpleuart_reg_dat_sel && !mem_wstrb),
 		.reg_dat_di  (mem_wdata),
 		.reg_dat_do  (simpleuart_reg_dat_do),
-		.reg_dat_wait(simpleuart_reg_dat_wait)
+		.reg_dat_wait(simpleuart_reg_dat_wait),
 			       );
    
-   reg wire dsd;
+   wire dsd, dsd2;
    assign audio_l = {dsd, 3'b0};
-   assign audio_r = {dsd, 3'b0};
+   assign audio_r = {dsd2, 3'b0};
 
         audio audio (.clk(clk),
-		     .dsd(dsd),
+		     .dsd(dsd), .dsd2(dsd2),
 		     .resetn(resetn),
 		     .sel    (dac_sel),
 		     .ready  (dac_ready),
@@ -261,7 +275,7 @@ module picosoc_regs (
 	input [5:0] raddr2,
 	input [31:0] wdata,
 	output [31:0] rdata1,
-	output [31:0] rdata2
+	output [31:0] rdata2,
 );
 	reg [31:0] regs [0:31];
 
@@ -272,8 +286,8 @@ module picosoc_regs (
 	assign rdata2 = regs[raddr2[4:0]];
 endmodule
 
- // 50 MHz
-module pll(input clki, 
+ // 40 MHz
+module pll(input clki,
     output locked,
     output clko
 );
@@ -287,12 +301,12 @@ EHXPLLL #(
         .STDBY_ENABLE("DISABLED"),
         .DPHASE_SOURCE("DISABLED"),
         .CLKOP_FPHASE(0),
-        .CLKOP_CPHASE(5),
+        .CLKOP_CPHASE(7),
         .OUTDIVIDER_MUXA("DIVA"),
         .CLKOP_ENABLE("ENABLED"),
-        .CLKOP_DIV(12),
-        .CLKFB_DIV(2),
-        .CLKI_DIV(1),
+        .CLKOP_DIV(15),
+        .CLKFB_DIV(8),
+        .CLKI_DIV(5),
         .FEEDBK_PATH("INT_OP")
     ) pll_i (
         .CLKI(clki),
@@ -318,16 +332,17 @@ module ulx3s(
     output ftdi_rxd,
     input ftdi_txd,
     output wifi_gpio0,
-    output [3:0] audio_l,
-    output [3:0] audio_r,
-    output [3:0] gpdi_dp, gpdi_dn,
 
     output flash_csn,
-    output flash_clk,
-    input flash_mosi,
+    output flash_mosi,
     input flash_miso,
-    input flash_holdn,
-    input flash_wpn,
+    output flash_wpn,
+    output flash_holdn,
+
+    output [3:0] audio_l,
+    output [3:0] audio_r,
+    
+    output [3:0] gpdi_dp, gpdi_dn,
 );
    
    assign wifi_gpio0 = 1'b1;
@@ -339,22 +354,55 @@ pll pll(
 );
 
 
+  wire flash_sck;
+  wire tristate = 1'b0;
+  USRMCLK u1 (.USRMCLKI(flash_sck), .USRMCLKTS(tristate));
+
+
+   wire flash_io0_oe, flash_io0_do, flash_io0_di;
+   wire flash_io1_oe, flash_io1_do, flash_io1_di;
+   wire flash_io2_oe, flash_io2_do, flash_io2_di;
+   wire flash_io3_oe, flash_io3_do, flash_io3_di;
+
+   TRELLIS_IO #(.DIR("BIDIR"))
+   flash_io_buffer0 [3:0] (
+      .B({flash_mosi, flash_miso, flash_wpn, flash_holdn}),
+      .I({flash_io0_do, flash_io1_do, flash_io2_do, flash_io3_do}),
+      .O({flash_io0_di, flash_io1_di, flash_io2_di, flash_io3_di}),
+      .T({!flash_io0_oe, !flash_io1_oe, !flash_io2_oe, !flash_io3_oe}));
+   
 attosoc soc(
     .clk(clk),
     .clk_25mhz(clk_25mhz),
     .led(led),
+    
     .uart_tx(ftdi_rxd),
     .uart_rx(ftdi_txd),
+    
     .audio_l(audio_l),
     .audio_r(audio_r),
+    
     .gpdi_dp(gpdi_dp),
     .gpdi_dn(gpdi_dn),
+
     .flash_csn(flash_csn),
-    .flash_clk(flash_clk),
-    .flash_io0(flash_mosi),
-    .flash_io1(flash_miso),
-    .flash_io2(flash_holdn),
-    .flash_io3(flash_wpn),
+    .flash_sck(flash_sck),
+//    .flash_mosi(flash_mosi),
+//    .flash_miso(flash_miso),
+    .flash_io0_oe (flash_io0_oe),
+    .flash_io1_oe (flash_io1_oe),
+    .flash_io2_oe (flash_io2_oe),
+    .flash_io3_oe (flash_io3_oe),
+    
+    .flash_io0_do (flash_io0_do),
+    .flash_io1_do (flash_io1_do),
+    .flash_io2_do (flash_io2_do),
+    .flash_io3_do (flash_io3_do),
+
+    .flash_io0_di (flash_io0_di),
+    .flash_io1_di (flash_io1_di),
+    .flash_io2_di (flash_io2_di),
+    .flash_io3_di (flash_io3_di),
 );
 endmodule
 
